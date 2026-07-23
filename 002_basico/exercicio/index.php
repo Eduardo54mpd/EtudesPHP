5d3c1e748d944f4c032dff43892d9ab993ceab7f
Eduardo54mpd <emm3574@gmail.com>
2026-07-23 20:44:15 -0300

    exercicio

---
+++ 002_basico/exercicio/index.php
@@ -0,0 +1,9 @@
+<?php 
+    // Nome do indivíduo
+    $nome = "Mikhail Petrovich Dmitriev";
+
+    // Local de origem
+    $origem = "Rússia";
+
+    // Ocupação do indivíduo
+    $ocupacao = ["Fazendeiro"];
